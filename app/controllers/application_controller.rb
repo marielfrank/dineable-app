28a4f2a14353b547043bc2970bf7f59c38de143c
Mariel <marielbeth@gmail.com>
2017-09-25 20:45:05 +0000

    Add application controller with configuration & helper methods

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    # Set up sessions
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  # allow visit to homepage
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    # get current user as object
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    # determin if user is logged in
+    def logged_in?
+      # check if current session has a user_id
+      !!session[:user_id]
+    end
+  end
+end
