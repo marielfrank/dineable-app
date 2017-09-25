fbb16fd32edf27a8e1956f6117ec4e60f4064e23
Mariel <marielbeth@gmail.com>
2017-09-25 22:25:54 +0000

    Fix migration file names

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170925213458) do
+
+  create_table "diet_prefs", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "restaurant_diet_prefs", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "diet_pref_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
 
 end
