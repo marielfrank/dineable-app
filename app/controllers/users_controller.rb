c23f625da4f6bdcbcab192a1dcce6d6b351fe46c
Mariel <marielbeth@gmail.com>
2017-09-26 14:41:12 +0000

    Add view layout, homepage, & signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UserController < ApplicationController
+  # allow visit to signup page unless logged in
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect '/restaurants'
+    end
+  end
 
+  # signup form action to create new user
+  post '/signup' do
+    @user = User.new(username: params[:username], password: params[:password])
+
+    if !params[:username].empty? && !params[:password].empty? && @user.save
+      session[:user_id] = @user.id
+    else
+      redirect '/signup'
+    end
+  end
+
+  # allow visit to login page unless logged in
+  get '/login' do
+
+  end
+
+  # logout
+  post '/logout' do
+
+  end
 end
