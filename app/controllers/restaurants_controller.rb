6db6748e9d6b6e69d921d8f2684834b389c444dc
Mariel <marielbeth@gmail.com>
2017-09-26 21:36:22 +0000

    Add restaurant create page and routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,12 +3,32 @@ class RestaurantController < ApplicationController
     erb :'restaurants/index'
   end
 
+  get '/restaurants/new' do
+    if logged_in?
+      erb :'/restaurants/new'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/restaurants/:slug' do
     @restaurant = Restaurant.find_by_slug(params[:slug])
     if !!@restaurant
       erb :'restaurants/show'
     else
       redirect '/restaurants'
+    end
+  end
+
+  post '/restaurants' do
+    if !params[:restaurant][:name].empty?
+      @restaurant = Restaurant.create(params[:restaurant])
+      @restaurant.user = current_user
+      @restaurant.diet_prefs << DietPref.new(name: params[:diet_pref])
+      @restaurant.save
+      redirect "/restaurants/<%= @restaurant.slug %>"
+    else
+      redirect '/restaurants/new'
     end
   end
 end
