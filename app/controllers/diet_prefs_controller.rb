a2ea92fba6c2c6221b68ab9eadfb871932d279b1
Mariel <marielbeth@gmail.com>
2017-09-27 14:19:06 +0000

    Add dietary preferences index and show views & routes

--- app/controllers/diet_prefs_controller.rb
+++ app/controllers/diet_prefs_controller.rb
@@ -1,3 +1,10 @@
 class DietPrefController < ApplicationController
+  get '/dietary-preferences' do
+    erb :'diet_prefs/index'
+  end
 
+  get '/dietary-preferences/:slug' do
+    @dietpref = DietPref.find_by_slug(params[:slug])
+    erb :'diet_prefs/show'
+  end
 end
